dbe48fc4f3cb9cc772591162d0cc0b683b9a2619
Rohit <ts.rohitsen@gmail.com>
2024-12-04 21:49:27 +0600

    Rohit - added methods to get all relevent data from AI and merge them in a JSON OBJ

--- src/utils/getAI.js
+++ src/utils/getAI.js
@@ -1,25 +1,19 @@
-
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-
 export const getResponse = async (prompt) => {
-  const GEMINI_API_KEY = process.env.NEXT_PUBLIC_GEMINI_API_KEY; 
+  const GEMINI_API_KEY = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
 
-    try {
-
-        const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
-        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
-
+  try {
+    const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
     let res = await model.generateContent(prompt);
-  //  console.log(response.response.candidates[0].content.parts[0].text)
-  const cleanedString = await res.response.candidates[0].content.parts[0].text;
+    const cleanedString = await res.response.candidates[0].content.parts[0]
+      .text;
 
-  return cleanedString;
-
-} catch(err) {
-    console.error(err)
-}
-return null;
-
-}
+    return cleanedString;
+  } catch (err) {
+    console.error(err);
+  }
+  return null;
+};
