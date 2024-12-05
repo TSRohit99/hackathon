1e3869fa5932f25cc2371c6c279c793a0499c7fa
Sadiqul Islam Shakib <sadiqul.islam.shakib21@gmail.com>
2024-12-05 14:53:59 +0600

    shakib

--- src/components/demo_component/page.jsx
+++ src/components/demo_component/page.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function Page() {
   return (
     <div>
-      <h1 className='text-red-700 font-bold shadow-2xl'>Rohit Sen</h1>
+      <h1 className='text-red-700 font-bold shadow-2xl'>Shakib</h1>
+      {/* <h1 className='text-red-700 font-bold shadow-2xl'>Rohit Sen</h1> */}
       <h1 className='text-red-700 font-bold shadow-2xl'>NEUB</h1>
     </div>
   )
