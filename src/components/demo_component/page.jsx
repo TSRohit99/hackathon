11c1c13ac9166792c5489618c9b1945e6a510cec
Mashiat Islam <mashiath342@gmail.com>
2024-12-05 14:23:26 +0600

    changed demo component

--- src/components/demo_component/page.jsx
+++ src/components/demo_component/page.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 export default function Demo() {
   return (
     <div>
-        Mashiat
-      {/* <h1 className='h-'>Mashiat</h1> */}
+      <h1 className='text-red-700 font-bold shadow-2xl'>Mashiat islam</h1>
     </div>
   )
 }
