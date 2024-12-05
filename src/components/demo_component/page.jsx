8c484c375398df63b41f11da2478116c228f39c5
Rohit <ts.rohitsen@gmail.com>
2024-12-05 14:39:23 +0600

    Merge Test

--- src/components/demo_component/page.jsx
+++ src/components/demo_component/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Demo() {
   return (
     <div>
-      <h1 className='text-red-700 font-bold shadow-2xl'>Mashiat islam</h1>
+      <h1 className='text-red-700 font-bold shadow-2xl'>Rohit islam</h1>
     </div>
   )
 }
