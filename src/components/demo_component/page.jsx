137f32ef15197db797b5d2238bc0cd8b07509f87
Sadiqul Islam Shakib <sadiqul.islam.shakib21@gmail.com>
2024-12-05 14:42:58 +0600

    shakib - demo conflick

--- src/components/demo_component/page.jsx
+++ src/components/demo_component/page.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-export default function Demo() {
+export default function Page() {
   return (
     <div>
-      <h1 className='text-red-700 font-bold shadow-2xl'>Mashiat islam</h1>
+      <h1 className='text-red-700 font-bold shadow-2xl'>Sadiqul Islam Shakib</h1>
+      <h1 className='text-red-700 font-bold shadow-2xl'>Sadiqul Islam Shakib</h1>
+      <h1 className='text-red-700 font-bold shadow-2xl'>Sadiqul Islam Shakib</h1>
+      <h1 className='text-red-700 font-bold shadow-2xl'>Sadiqul Islam Shakib</h1>
+      <h1 className='text-red-700 font-bold shadow-2xl'>Sadiqul Islam Shakib</h1>
+      <h1 className='text-red-700 font-bold shadow-2xl'>Sadiqul Islam Shakib</h1>
     </div>
   )
 }
